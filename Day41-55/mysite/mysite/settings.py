54b1693bba31fa4fcd7ea4e3b288cf2e16e802e2
Jerrylee <link51inte@hotmail.com>
2019-06-26 11:43:18 +0800

    数据库配置，执行“python manage.py migrate”自动创建表

--- Day41-55/mysite/mysite/settings.py
+++ Day41-55/mysite/mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'djuser',
+        'PASSWORD': '163.com..',
+        'HOST': '192.168.84.242',
+        'PORT': '3306'
     }
 }
 
