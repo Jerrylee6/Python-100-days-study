ccb3737ef9cc8f1f933688a80d36a62846de7514
Jerrylee <link51inte@hotmail.com>
2019-07-30 20:28:36 +0800

    django用户登录与注册系统, （登录视图）1.数据验证，2.添加提示信息。注意这里验证的表名来源于"login_user"，通过"views.py"文件内的"user = models.User.objects.get(name=username)"指定

--- Day41-55/mysite_login/login/views.py
+++ Day41-55/mysite_login/login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render,redirect
+from . import models
 
 def index(request):
     pass
@@ -9,9 +10,23 @@ def index(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        return redirect('/index/')
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        message = "所有字段都必须填写！"
+        if username and password:  # 确保用户名和密码都不为空
+            username = username.strip()
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户名不存在！"
+        return render(request, 'login/login.html', {"message": message})
     return render(request, 'login/login.html')
 
 def register(request):
