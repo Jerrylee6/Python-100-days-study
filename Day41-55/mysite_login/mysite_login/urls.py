626e98e3ca12d189383aa31b3aae2d26c59967c1
Jerrylee <link51inte@hotmail.com>
2019-07-31 20:02:44 +0800

    django用户登录与注册系统, （session会话) 1.完善页面(结果，有用户时进入登录视图、未登录时进入公共视图)

--- Day41-55/mysite_login/mysite_login/urls.py
+++ Day41-55/mysite_login/mysite_login/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/', views.index),
     url(r'^login/', views.login),
-    url(r'^register/', views.register),
+    # url(r'^register/', views.register),
     url(r'^logout/', views.logout),
     url(r'^captcha', include('captcha.urls'))
 ]
