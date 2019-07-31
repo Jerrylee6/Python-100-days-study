f7d1d241734940f173904befd96cde4b61dc7188
Jerrylee <link51inte@hotmail.com>
2019-07-31 19:46:10 +0800

    django用户登录与注册系统, （session会话) 1.使用session(修改login/views.py)完成登录视图和登出视图

--- Day41-55/mysite_login/login/views.py
+++ Day41-55/mysite_login/login/views.py
@@ -17,6 +17,9 @@ def index(request):
     return render(request,'login/index.html')
 
 def login(request):
+    if request.session.get('is_login',None):
+        return redirect('/index')
+
     if request.method == "POST":
         login_form = UserForm(request.POST)
         message = "请检查填写的内容！"
@@ -26,6 +29,9 @@ def login(request):
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
                     return redirect('/index/')
                 else:
                     message = "密码不正确！"
@@ -36,10 +42,13 @@ def login(request):
     login_form = UserForm()
     return render(request, 'login/login.html', locals())
 
-def register(request):
-    pass
-    return render(request,'login/register.html')
-
 def logout(request):
-    pass
-    return redirect('/index/')
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/index/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    return redirect("/index/")
